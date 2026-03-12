8d0569d6b9eaa54e3fec846008f4066a913018f9
ishant thakkar <ishant.thakkar@metizsoft.com>
2026-03-12 17:21:43 +0530

    update DB url 3

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     JWT_SECRET: process.env.JWT_SECRET || "your_jwt_secret_key",
-    MONGO_URI: process.env.MONGO_URI || "mongodb+srv://ishantthakkar_db_user:yWGpF8dBcSp2xv1K@cluster0.kovdwcb.mongodb.net/caip?appName=Cluster0",
+    MONGO_URI: process.env.MONGO_URI || "mongodb+srv://ishantthakkar_db_user:yWGpF8dBcSp2xv1K@cluster0.kovdwcb.mongodb.net/caip?retryWrites=true&w=majority",
     PORT: process.env.PORT || 5000
 };
