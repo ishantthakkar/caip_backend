05033eb3ab746f821f8401af1a02d79e83441fec
ishant thakkar <ishant.thakkar@metizsoft.com>
2026-03-12 16:34:18 +0530

    change DB url

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     JWT_SECRET: process.env.JWT_SECRET || "your_jwt_secret_key",
-    MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/caip",
+    MONGO_URI: process.env.MONGO_URI || "mongodb+srv://ishantthakkar_db_user:yWGpF8dBcSp2xv1K@cluster0.kovdwcb.mongodb.net/?appName=Cluster0",
     PORT: process.env.PORT || 5000
 };
