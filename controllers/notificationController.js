b70111d27fc490bfc6b0cc13c360532826194dff
ishant thakkar <ishant.thakkar@metizsoft.com>
2026-08-14 14:17:36 +0530

    Solved the notification issue of listing

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -89,7 +89,6 @@ exports.getMobileNotifications = async (req, res) => {
                 {
                     $or: [
                         { member_id: 'All' },
-                        { member_id: userId }
                     ]
                 },
                 {
